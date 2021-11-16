d43f9e6167e8040a7c90f0514a6b988e1724ecc6
runnaaway <smkate.work@gmail.com>
2021-11-17 01:26:46 +0300

    Add donation block

--- src/tpl/pages/index/index.js
+++ src/tpl/pages/index/index.js
@@ -1,8 +1,20 @@
 window.onload = function () {
-    document.querySelector('.form__head-filter__status .status__lnk').onclick = function (e) {
+    let formHeadFilterStatus = document.querySelector('.form__head-filter__status');
+
+    document.querySelector('.form__head-filter__status .status__lnk').addEventListener('click', function (e) {
         e.preventDefault();
-        this.parentNode.classList.toggle('vis')
-    }
+        formHeadFilterStatus.classList.toggle('vis')
+    });
+
+    window.addEventListener('click', function () {
+        formHeadFilterStatus.classList.remove('vis');
+    });
+
+    formHeadFilterStatus.addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+
+    /********/
 
     let menu = document.querySelector('.menu');
     let menuOffset = menu.offsetTop;
@@ -14,4 +26,25 @@ window.onload = function () {
             menu.classList.remove('fixed')
         }
     });
+
+    /*******/
+
+    let copyDonationIDLnk = document.querySelector('.donation__id-copy');
+    let copyDonationTooltip = document.querySelector('.donation__id-copy-tooltip');
+    let donationID = document.querySelector('.donation__id-val').textContent;
+
+    copyDonationIDLnk.addEventListener('click', function (e) {
+        e.preventDefault();
+        this.classList.add('copied');
+        navigator.clipboard.writeText(donationID);
+        copyDonationTooltip.textContent = 'Copied!';
+    });
+
+    copyDonationIDLnk.addEventListener('mouseout', function () {
+        let _this = this;
+        setTimeout(function () {
+            _this.classList.remove('copied');
+            copyDonationTooltip.textContent = 'Copy to clipboard';
+        }, 1000)
+    })
 }
