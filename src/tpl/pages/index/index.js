215e9b214b162afd667e14246f806547d321a405
runnaaway <smkate.work@gmail.com>
2021-11-17 00:29:26 +0300

    Add aside menu

--- src/tpl/pages/index/index.js
+++ src/tpl/pages/index/index.js
@@ -3,4 +3,15 @@ window.onload = function () {
         e.preventDefault();
         this.parentNode.classList.toggle('vis')
     }
+
+    let menu = document.querySelector('.menu');
+    let menuOffset = menu.offsetTop;
+
+    window.addEventListener('scroll', function(e) {
+        if (window.pageYOffset >= menuOffset + 30 ) {
+            menu.classList.add('fixed')
+        } else {
+            menu.classList.remove('fixed')
+        }
+    });
 }
